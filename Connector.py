06fabd8689f8ad819c2b1451b25339eec113f9ce
sqelb <110704332+sqelb@users.noreply.github.com>
2024-12-07 19:36:13 +0000

    Update Connector.py
    The base ConnectionHandler frame

--- Connector.py
+++ Connector.py
@@ -1,1 +1,12 @@
+from netmiko import ConnectHandler # adds a pre compiled libary called NetMiko which handles SSH connection to other machines through python, in specific importing the ConnectionHandler
+# importing only a certain section of NetMiko ensures a smaller file size as the whole libary is then not imported
+
+NetConnect = ConnectHandler(
+device_type= autodetect,
+host= IP,
+port= "5679" PORT,
+username= USER, # Change to your username
+password= PASS, # Change to your password
+)
+
 
