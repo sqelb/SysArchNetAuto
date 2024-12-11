175563278ead456f9647254a6f897fdcb340148e
sqelb <mantasproe@gmail.com>
2024-12-11 19:43:38 +0000

    Added getpass and fixed autodetect

--- Connector.py
+++ Connector.py
@@ -1,19 +1,23 @@
 from netmiko import ConnectHandler 
+import getpass # Secure way of inputting a password (no terminal feedback)
 '''
 adds a pre compiled libary called NetMiko which handles
 SSH connection to other machines through python, 
 in specific importing the ConnectionHandler
 '''
-# importing only a certain section of NetMiko ensures a smaller file size as the whole libary is then not imported
+'''
+importing only a certain section of NetMiko ensures a smaller file size 
+as the whole libary is then not imported
+'''
 # NetMiko is an extension of ParaMiko with a lot more usability
 
 IP = input(str("What IP are you attempting to connect to?"))
 USER = input(str("What is the Username of the machine you're attempting to connect to?"))
-PASS = input(str("What is the Password of the machine you're attempting to connect to?"))
+PASS = getpass.getpass("What is the Password of the machine you're attempting to connect to?")
 # Inputs to the parameters needed to establish the connection
 
 NetConnect = ConnectHandler(
-                            device_type= autodetect, # The device OS your attempting to connect to
+                            device_type= "autodetect", # The device OS your attempting to connect to
                             host= IP, # is the IP of the Host you're attempting to connect to
                             port= "5679", # is a Host port whereas port 22 is a Guest PORT
                             username= USER, # Change to your username
--- handler.py
+++ handler.py
@@ -1,12 +1,13 @@
 
 
-r, c = 5, 1 # Defining how many rows and columns the array has 
+r, c = 6, 1 # Defining how many rows and columns the array has 
 Matrix = [[0 for x in range(r)] for y in range(c)] # Creates a 2D Array
 Matrix[0][0] = "Show date and time"
 Matrix[0][1] = "Show IP address"
 Matrix[0][2] = "Show Remote home directory listing"
 Matrix[0][3] = "Backup remote file"
-Matrix[0][4] = "Quit"
+Matrix[0][4] = "Save web page"
+Matrix[0][5] = "Quit"
 '''
  All the options available for the user, once an option has been 
  selected by the user the code for it would be executed.
@@ -25,28 +26,41 @@ def options(i, Matrix, r): # Function created to output all the primary options 
             print(i, ": ", "{}".format(val)) 
             # Using formating to output the Option numbers and the options themselves
 
-options(i, Matrix, r) # Calling function to print out all the options
 
-mon = True
-while mon == True:
+
+while True:
+    options(i, Matrix, r) # Calling function to print out all the options
     q = int(input("What Option do you pick?\n")) # Getting user input for primary option
+    q -= q
 
     # Based on user input detects all the available primary options and gives the desired output 
-    if q == 1:
+    if q == 0:
         print("You have picked: ", Matrix[0][q])
-        mon = False
+        command = "date"
+        break
+
+    elif q == 1:
+        print("You have picked: ", Matrix[0][q])
+        command = "ip addr show"
+        break
+
     elif q == 2:
         print("You have picked: ", Matrix[0][q])
-        mon = False
+        command = "ls ~/"
+        break
+
     elif q == 3:
         print("You have picked: ", Matrix[0][q])
-        mon = False
+        path = input("What Directory do you wish to copy from?\n~/")
+        file = input("What file do you wish to copy? ")
+        command = "cp ~/{path}{file} ~/{path}{file}".format()
+        break
+
     elif q == 4:
         print("You have picked: ", Matrix[0][q])
-        mon = False
-    elif q == 4:
-        print("You have picked: ", Matrix[0][q])
-        mon = False
+
+        break
+
     else:
         print("You have picked an invalid value\n\n") # Error message if no valid input was detected
-        options(i, Matrix, r) # Prints options again to remind user of the options
+        
